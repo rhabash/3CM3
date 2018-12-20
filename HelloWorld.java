0f1c580895dc98d01c9b54e2a667483cf7a4864c
Razanne Habash <razannehabash@Razannes-MacBook-Pro-2.local>
2018-12-19 21:31:55 -0500

    Final HelloWorld Code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
-
+        int x = 0;
         //Here is where your code goes
         System.out.println("Hello, World!");
+        System.out.println("I am superior");
+        x = 10;
+        System.out.println("300");
+        x = 15;
+        System.out.println("I am tired");
         
     }
 }
